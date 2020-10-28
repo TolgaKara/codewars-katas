43f15b0a86187e728dba939a4c91b0ba84bb6d7b
Tolga Kara <tolgakara2.0@gmail.com>
2020-10-28 06:43:23 +0100

    Get Planet Name By ID

--- 8 kyu/index.js
+++ 8 kyu/index.js
@@ -165,3 +165,36 @@ function areYouPlayingBanjo(name) {
 function check(a, x) {
 	return a.includes(x)
 }
+
+// Get Planet Name By ID
+function getPlanetName(id) {
+	var name
+	switch (id) {
+		case 1:
+			name = "Mercury"
+			break
+		case 2:
+			name = "Venus"
+			break
+		case 3:
+			name = "Earth"
+			break
+		case 4:
+			name = "Mars"
+			break
+		case 5:
+			name = "Jupiter"
+			break
+		case 6:
+			name = "Saturn"
+			break
+		case 7:
+			name = "Uranus"
+			break
+		case 8:
+			name = "Neptune"
+			break
+	}
+
+	return name
+}
