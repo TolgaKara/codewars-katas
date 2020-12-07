8b4c63b6d34c68246f0c10b4d7df700c14e150f3
TolgaKara <tolgakara2.0@gmail.com>
2020-12-07 11:36:26 +0100

    Turkish Numbers, 0-99

--- 7 kyu/index.js
+++ 7 kyu/index.js
@@ -263,3 +263,56 @@ function boredom(staff) {
 		return "party time!!";
 	}
 }
+
+// Turkish Numbers, 0-99
+const getTurkishNumber = (num) => {
+	num = num.toString();
+	let word = "";
+	const number = {
+		0: "sıfır",
+		1: "bir",
+		2: "iki",
+		3: "üç",
+		4: "dört",
+		5: "beş",
+		6: "altı",
+		7: "yedi",
+		8: "sekiz",
+		9: "dokuz",
+	};
+	const tensNumber = {
+		10: "on",
+		20: "yirmi",
+		30: "otuz",
+		40: "kırk",
+		50: "elli",
+		60: "altmış",
+		70: "yetmiş",
+		80: "seksen",
+		90: "doksan",
+	};
+
+	if (num < 10) {
+		for (let key in number) {
+			if (num === key) {
+				return number[key];
+			}
+		}
+	} else {
+		for (let tensKey in tensNumber) {
+			if (num[0] == tensKey[0]) {
+				word += tensNumber[tensKey];
+			}
+		}
+
+		for (let key in number) {
+			if (num[1] === key) {
+				if (num[1] === "0") {
+					break;
+				}
+				word += " " + number[key];
+			}
+		}
+	}
+	return word;
+};
